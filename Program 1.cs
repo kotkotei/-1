1f465e3b1ac3ebb56d4035680304328f4afd118e
kotkotei <kotkotei@yandex.ru>
2021-05-15 10:51:59 +0700

    vfjdeijdiohjor

---
+++ Program 1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число a");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число n1");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число a1");
+            double a1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число n2");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+            double an1a1n2 = (a * n1 + a1 * n2) / 100;
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var r = 4;
+            var result = (1 - Math.PI * 4 * r * r * r / 3 / a / a / a) * 100;
+            Console.WriteLine(result.ToString("#.##"));
+            Console.ReadKey();
+
+        }
+    }
+}
